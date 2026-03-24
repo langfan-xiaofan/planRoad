08b19b5727ce88f921feb15a0f56c58b10d14762
2824920336 <2824920336@qq.com>
2026-03-24 23:37:55 +0800

    增加了agent功能

--- backend/cmd/cmd.go
+++ backend/cmd/cmd.go
@@ -4,12 +4,54 @@ import (
 	"backend/internal/config"
 	"backend/internal/db"
 	"backend/internal/router"
+	"backend/pkg/agent"
+	"context"
+	"fmt"
+	"os"
 
+	eino "github.com/cloudwego/eino-ext/components/model/openai"
+	"github.com/cloudwego/eino/compose"
+	"github.com/cloudwego/eino/flow/agent/react"
+	"github.com/cloudwego/eino/schema"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func Run() {
-	err := config.Init()
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	agentConfig := eino.ChatModelConfig{
+		APIKey:  os.Getenv("API_KEY"),
+		Model:   os.Getenv("Model"),
+		BaseURL: os.Getenv("BaseURL"),
+	}
+	client, err := eino.NewChatModel(context.Background(), &agentConfig)
+	if err != nil {
+		panic(err)
+	}
+	tools := agent.CreateTool()
+	toolInfos := make([]*schema.ToolInfo, 0)
+	for _, tool := range tools {
+		info, err := tool.Info(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		toolInfos = append(toolInfos, info)
+	}
+	client.BindTools(toolInfos)
+	agent, err := react.NewAgent(context.Background(), &react.AgentConfig{
+		Model: client,
+		ToolsConfig: compose.ToolsNodeConfig{
+			Tools: tools,
+		},
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = config.Init()
 	if err != nil {
 		panic(err)
 	}
@@ -30,7 +72,7 @@ func Run() {
 		panic(err)
 	}
 	r := gin.Default()
-	router.InitRouter(r)
+	router.InitRouter(r, agent)
 	err = r.Run(":8080")
 	if err != nil {
 		return
--- backend/internal/router/routers.go
+++ backend/internal/router/routers.go
@@ -1,7 +1,11 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/cloudwego/eino/flow/agent/react"
+	"github.com/gin-gonic/gin"
+)
 
-func InitRouter(g *gin.Engine) {
+func InitRouter(g *gin.Engine, client *react.Agent) {
 	initUserRouter(g)
+	InitAgentRouter(g, client)
 }
