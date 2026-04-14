ab5b417a48b5e28585c50b16f0bacd865a7488e4
YN1753 <15347290648@163.com>
2026-04-14 21:17:28 +0800

    增加了获取岗位血缘关系的功能

--- backend/internal/model/job.go
+++ backend/internal/model/job.go
@@ -13,3 +13,12 @@ type Position struct {
 	ExperienceReq   string   `bson:"experience_req"`
 	PublicBase      []string `bson:"publicbase"`
 }
+
+type PositionAndAdvice struct {
+	PositionName string `bson:"position_name" json:"position_name"`
+	Advice       string `bson:"advice" json:"advice"`
+}
+type JobRelationship struct {
+	OriginPosition string              `bson:"origin_position" json:"origin_position"`
+	Suggestion     []PositionAndAdvice `bson:"suggestion" json:"suggestion"`
+}
